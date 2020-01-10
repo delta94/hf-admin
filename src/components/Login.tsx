ed8b2fffdb8398dfc28f7a10deb2268c96018978
sbinha0819 <sbinha123@gmail.com>
2020-01-10 14:05:45 +0900

    cookie token

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,7 +4,7 @@ import { useCookies } from 'react-cookie';
 import { useQuery } from '@apollo/react-hooks';
 import { GET_TOKEN } from '../graphql/queries';
 import { Input, Icon, Button, Form } from 'antd';
-import { on } from 'cluster';
+
 let query;
 
 function Head() {
@@ -16,7 +16,7 @@ function Head() {
 
   useEffect(() => {
     localStorage.setItem('isLogin', String(isLogin));
-  }, [isLogin]);
+  }, [isLogin, cookies]);
 
   const { data } = useQuery(GET_TOKEN, {
     variables: { ...query },
@@ -47,51 +47,46 @@ function Head() {
 
   return (
     <>
-      <Button type="primary" size="large">
-        <a href={'http://localhost:4000/auth/google'}>임시 admin button</a>
-      </Button>
-      <>
-        {isLogin ? (
-          <Form
-            style={{
-              position: 'absolute',
-              top: '20px',
-              right: '20px',
-              color: 'white',
+      {isLogin ? (
+        <Form
+          style={{
+            position: 'absolute',
+            top: '20px',
+            right: '20px',
+            color: 'white',
+          }}
+        >
+          <span style={{ marginRight: '20px' }}>반갑습니다</span>
+          <Button
+            onClick={() => {
+              onLogin();
             }}
           >
-            <span style={{ marginRight: '20px' }}>반갑습니다</span>
-            <Button
-              onClick={() => {
-                onLogin();
-              }}
-            >
-              Logout
-            </Button>
-          </Form>
-        ) : (
-          <Form style={{ position: 'absolute', top: '10px', right: '20px' }}>
-            <Input
-              prefix={<Icon type="user" />}
-              style={{ width: '150px', marginRight: '15px' }}
-              placeholder="Username"
-              value={username}
-              onChange={onChangeUsernmae}
-            />
-            <Input
-              prefix={<Icon type="lock" />}
-              style={{ width: '150px', marginRight: '15px' }}
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={onChangePassword}
-            />
-            <Button type="primary" size="large" onClick={() => onLogin()}>
-              Login
-            </Button>
-          </Form>
-        )}
-      </>
+            Logout
+          </Button>
+        </Form>
+      ) : (
+        <Form style={{ position: 'absolute', top: '10px', right: '20px' }}>
+          <Input
+            prefix={<Icon type="user" />}
+            style={{ width: '150px', marginRight: '15px' }}
+            placeholder="Username"
+            value={username}
+            onChange={onChangeUsernmae}
+          />
+          <Input
+            prefix={<Icon type="lock" />}
+            style={{ width: '150px', marginRight: '15px' }}
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={onChangePassword}
+          />
+          <Button type="primary" size="large" onClick={() => onLogin()}>
+            Login
+          </Button>
+        </Form>
+      )}
     </>
   );
 }
