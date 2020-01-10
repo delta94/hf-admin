a1eadf5af74737c37e553d8ba664bb90e2300c30
sbinha0819 <sbinha123@gmail.com>
2020-01-10 17:26:05 +0900

    reload page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,26 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import 'antd/dist/antd.css';
-import { useCookies } from 'react-cookie';
-import { useQuery } from '@apollo/react-hooks';
-import { GET_TOKEN } from '../graphql/queries';
 import { Input, Icon, Button, Form } from 'antd';
-
+import Test from './LoginQuery';
 let query;
 
 function Login() {
   const initIsLogin = () => Boolean(localStorage.getItem('isLogin') || false);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [cookies, setCookie] = useCookies(['name']);
   const [isLogin, setIsLogin] = useState(initIsLogin);
 
   useEffect(() => {
     localStorage.setItem('isLogin', String(isLogin));
-  }, [isLogin, cookies]);
-
-  const { data } = useQuery(GET_TOKEN, {
-    variables: { ...query },
-  });
+  }, [isLogin]);
 
   const onChangeUsernmae = (e) => {
     setUsername(e.target.value);
@@ -32,39 +24,34 @@ function Login() {
 
   const onLogin = () => {
     query = { email: username, password: password };
+    if (isLogin === true) {
+    }
     setIsLogin(!isLogin);
   };
-
-  const onCookie = (token) => {
-    setCookie('access-token', token);
-  };
-
-  if (data) {
-    if (data.login) {
-      onCookie(data.login.token);
-    }
-  }
 
   return (
     <>
       {isLogin ? (
-        <Form
-          style={{
-            position: 'absolute',
-            top: '20px',
-            right: '20px',
-            color: 'white',
-          }}
-        >
-          <span style={{ marginRight: '20px' }}>반갑습니다</span>
-          <Button
-            onClick={() => {
-              onLogin();
+        <>
+          <Form
+            style={{
+              position: 'absolute',
+              top: '20px',
+              right: '20px',
+              color: 'white',
             }}
           >
-            Logout
-          </Button>
-        </Form>
+            <span style={{ marginRight: '20px' }}>반갑습니다</span>
+            <Button
+              onClick={() => {
+                onLogin();
+              }}
+            >
+              Logout
+            </Button>
+            <Test query={query} />
+          </Form>
+        </>
       ) : (
         <Form style={{ position: 'absolute', top: '10px', right: '20px' }}>
           <Input
---
+++ src/components/LoginQuery.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { useQuery } from '@apollo/react-hooks';
+import { GET_TOKEN } from '../graphql/queries';
+import { useCookies } from 'react-cookie';
+
+function Test({ query }) {
+  const [cookies, setCookie] = useCookies(['name']);
+  const { data } = useQuery(GET_TOKEN, {
+    variables: { ...query },
+  });
+
+  const onCookie = (token) => {
+    setCookie('access-token', token);
+  };
+
+  if (data) {
+    if (data.login) {
+      onCookie(data.login.token);
+      window.location.reload();
+    }
+  }
+
+  return (
+    <>
+      {/* <span style={{ marginRight: '20px' }}>반갑습니다</span> */}
+      {/* <Button>Logout</Button> */}
+    </>
+  );
+}
+
+export default Test;
