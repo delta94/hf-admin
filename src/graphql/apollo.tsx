79671391799e2204519a747ba3a8bf3f7d95ab55
sbinha0819 <sbinha123@gmail.com>
2020-01-18 14:06:02 +0900

    ws setting

--- src/graphql/apollo.tsx
+++ src/graphql/apollo.tsx
@@ -1,12 +1,27 @@
-import ApolloClient from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
+// import ApolloClient from 'apollo-client';
+import { ApolloClient, HttpLink, split } from 'apollo-boost';
+// import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { getMainDefinition } from 'apollo-utilities';
 import Cookies from 'js-cookie';
 
-const httpLink = createHttpLink({
+import { WebSocketLink } from 'apollo-link-ws';
+
+// const httpLink = createHttpLink({
+//   uri: 'http://localhost:4000/graphql',
+//   credentials: 'include',
+// });
+const httpLink = new HttpLink({
   uri: 'http://localhost:4000/graphql',
   credentials: 'include',
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+  },
 });
 
 const authLink = setContext((_, { headers }) => {
@@ -20,8 +35,21 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  // link: authLink.concat(httpLink).concat(wsLink),
+  link: authLink.concat(link),
   cache: new InMemoryCache(),
 });
 
