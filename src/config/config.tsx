f216a580a5f1b87a9f23a8bbcbe41c25287cb6d5
sbinha0819 <sbinha123@gmail.com>
2020-02-07 12:12:40 +0900

    fixed untracked files

--- src/config/config.tsx
+++
@@ -1,8 +0,0 @@
-export const API_KEY = '7gfp3v3jzxev';
-//4rcx837szctw
-//7gfp3v3jzxev
-//js3yvpj5rsny
-
-export const SECRET =
-  '2r9f57cyapym62axq2uadzwbzehsqquxhrmngrhmjdqrzyajscz9enwe7dfdt7qv';
-//2r9f57cyapym62axq2uadzwbzehsqquxhrmngrhmjdqrzyajscz9enwe7dfdt7qv
